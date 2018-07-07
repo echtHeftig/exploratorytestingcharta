c30bcef886506de0da2c757a3681e673953fb3be
mk <mk@example.com>
2018-07-07 16:56:16 +0200

    Save createdCharta to internal ChartaList

--- src/main/java/charta/ChartaController.java
+++ src/main/java/charta/ChartaController.java
@@ -1,12 +1,12 @@
 package charta;
 
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.websocket.server.PathParam;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
@@ -15,10 +15,11 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 public class ChartaController {
 
     private final AtomicLong counter = new AtomicLong();
+    private List<Charta> chartaList = new ArrayList<>();
 
     @RequestMapping(value = "/charta/fixid", method = POST)
     public Charta createAndReturnCharta(@RequestParam(value = "id", defaultValue = "1") long id) {
-        return new Charta(id, "Charter Name", "Dies sind versch. Areas", new Calendar.Builder().setDate(2018, 7, 3).build(),
+        return new Charta(id, "Charter Name mit RandomId", "Dies sind versch. Areas", new Calendar.Builder().setDate(2018, 7, 3).build(),
                 "Susanna", "", 13, 25, 25, 40,
                 10, "It/is/a/path", "Dies ist eine Notiz", "Es gab noch ein paar weitere Moeglichkeiten",
                 "Dies ist ein Bug. Und noch ein Bug", "Issue 1, Noch ein Issue 2");
@@ -26,9 +27,12 @@ public class ChartaController {
 
     @RequestMapping(value = "/charta", method = POST)
     public Charta createAndReturnChartaGeneratedId() {
-        return new Charta(counter.incrementAndGet(), "Charter Name", "Dies sind versch. Areas", new Calendar.Builder().setDate(2018, 7, 3).build(),
+        Charta charta = new Charta(counter.incrementAndGet(), "Charter Name", "Dies sind versch. Areas", new Calendar.Builder().setDate(2018, 7, 3).build(),
                 "Susanna", "", 13, 25, 25, 40,
                 10, "It/is/a/path", "Dies ist eine Notiz", "Es gab noch ein paar weitere Moeglichkeiten",
                 "Dies ist ein Bug. Und noch ein Bug", "Issue 1, Noch ein Issue 2");
+        chartaList.add(charta);
+        return charta;
     }
+
 }
