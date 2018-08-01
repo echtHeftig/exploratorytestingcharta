6ba4dfc8976a2fc569184da3c7118ee826643250
mk <mk@example.com>
2018-08-01 23:51:05 +0200

    Deleted JUnit Jupiter (JUnit 5) -> instead the JUnit 4 dependency of spring-boot-starter-test is used

--- src/test/java/tests/ChartaApiTests.java
+++ src/test/java/tests/ChartaApiTests.java
@@ -3,20 +3,31 @@ package tests;
 import api.ChartaApi;
 import io.restassured.response.Response;
 import org.approvaltests.Approvals;
-import org.junit.jupiter.api.Test;
-
+import org.junit.Test;
 import java.io.File;
 import java.util.Objects;
 
 public class ChartaApiTests {
 
     @Test
-    void createChartaTestReturnsCorrectPayload() {
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(Objects.requireNonNull(classLoader.getResource("post/PostReturnsCorrectPayload.json"))
-                .getFile());
+    public void createChartaTestReturnsCorrectPayload() {
+        File file = getTestDataFile("post/PostReturnsCorrectPayload.json");
         Response response = ChartaApi.postCharta(file);
 
         Approvals.verify(response.getBody().prettyPrint());
+    }
+
+    @Test
+    public void createChartaTestReturnsCorrectPayloadSecondTest() {
+        File file = getTestDataFile("post/PostReturnsCorrectPayloadSecondTest.json");
+        Response response = ChartaApi.postCharta(file);
+
+        Approvals.verify(response.getBody().prettyPrint());
+    }
+
+    private File getTestDataFile(String pathToFile) {
+        ClassLoader classLoader = getClass().getClassLoader();
+        return new File(Objects.requireNonNull(classLoader.getResource(pathToFile))
+                .getFile());
     }
 }
