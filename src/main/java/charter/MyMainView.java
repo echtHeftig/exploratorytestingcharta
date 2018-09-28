89687347dccd65b916d87bfb93efeded45dac638
mk <mk@example.com>
2018-09-28 17:57:35 +0200

    Adds delete Button for each Charter row (WIP: needs delete-confirmation dialog) that immediately removes the charter

--- src/main/java/charter/MyMainView.java
+++ src/main/java/charter/MyMainView.java
@@ -1,10 +1,13 @@
 package charter;
 
+import com.vaadin.icons.VaadinIcons;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.Grid;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringUI(path = "")
@@ -13,15 +16,29 @@ public class MyMainView extends UI {
     @Autowired
     private CharterRepository charterRepository;
 
+    private Grid<Charter> grid = new Grid<>(Charter.class);
+
     @Override
     protected void init(VaadinRequest request) {
         VerticalLayout verticalLayout = new VerticalLayout();
-        Grid<Charter> grid = new Grid<>(Charter.class);
         grid.setItems(charterRepository.findAll());
         grid.setWidth(100, Unit.PERCENTAGE);
-        grid.setColumnReorderingAllowed(true);
+        grid.setColumns("id", "charterName", "nameOfTester", "areas");
         grid.setColumnOrder("id", "charterName", "nameOfTester");
+        grid.addComponentColumn(this::buildDeleteButton);
         verticalLayout.addComponent(grid);
         setContent(verticalLayout);
+    }
+
+    private Button buildDeleteButton(Charter p) {
+        Button button = new Button(VaadinIcons.CLOSE);
+        button.addStyleName(ValoTheme.BUTTON_SMALL);
+        button.addClickListener(e -> deleteCharter(p));
+        return button;
+    }
+
+    private void deleteCharter(Charter p) {
+        charterRepository.delete(p);
+        grid.setItems(charterRepository.findAll());
     }
 }
